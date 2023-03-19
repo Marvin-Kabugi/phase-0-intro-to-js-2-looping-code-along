8f404fa39f372a3d3f193338f264f6e31d144610
J.M <kabugijeff@gmail.com>
2023-03-19 08:41:14 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arr, e){
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++){
+        let element = `Thank you, ${arr[i]}, for the wonderful ${e} gift!`;
+        newArr.push(element);
+    }
+    return newArr;
+}
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int);
+        int--;
+    }
+}
